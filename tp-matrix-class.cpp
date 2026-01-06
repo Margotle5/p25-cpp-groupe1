161958315af62b07669bd936c4e6be78a164f3db
Margot <mlecinq@gmail.com>
2026-01-06 15:56:32 +0100

    fin tp-matrix et création de tp-binary-tree

--- tp-matrix-class.cpp
+++ tp-matrix-class.cpp
@@ -5,13 +5,14 @@
 
 class Buffer
 {
-    friend class Matrix;
-
-    double *tab;
-    int nb_matrix;
+    friend class Matrix; // Les classes amis peuvent accéder à tous le contenu de Buffer
+private:
     int size;
-    
+    double *tab;
+    int nb_matrix; // nombre de Matrix relier au Buffer
+
 public:
+    // Création du tableau dans la mémoire dynamique
     Buffer(int size) : nb_matrix(0), size(size)
     {
         tab = new double[size];
@@ -24,7 +25,29 @@ public:
         {
             std::cout << tab[i];
         }
-        std::cout << std::endl;
+        std::cout << ']' << std::endl;
+    }
+
+    bool valid_index(int i)
+    {
+        return (i >= 0) and (i < size);
+    }
+
+    void set(int i, double value) // modifier la valeur d'un élément particulier
+    {
+        if (valid_index(i))
+            tab[i] = value;
+    }
+
+    double get(int i) // récupérer la valeur d'un élément particulier
+    {
+        if (valid_index(i))
+            return tab[i];
+    }
+
+    void incr_nb_matrix()
+    {
+        nb_matrix = nb_matrix + 1;
     }
 
     ~Buffer()
@@ -35,12 +58,19 @@ public:
 
 class Matrix
 {
-    friend class Buffer;
     Buffer *buffer_ptr;
     int rows;
     int columns;
 
 public:
+    int flatten_indices(int i, int j) {
+        return i*columns + j;
+    }
+
+    bool valid_indices(int i, int j) {
+        return ((i>=0) and (i<rows) and (j>=0) and (j<columns));
+    }
+
     Matrix(int r, int c) : rows(r), columns(c)
     {
         buffer_ptr = new Buffer(rows * columns);
@@ -49,7 +79,7 @@ public:
 
     Matrix(int r, int c, Buffer *buffer_ptr) : rows(r), columns(c), buffer_ptr(buffer_ptr)
     {
-        buffer_ptr->nb_matrix = buffer_ptr->nb_matrix + 1;
+        buffer_ptr->nb_matrix = buffer_ptr->nb_matrix + 1; // ou buffer->incr_nb_matrix()
     }
 
     void print()
@@ -59,7 +89,7 @@ public:
         {
             for (int j = 0; j < columns; j++)
             {
-                std::cout << buffer_ptr->tab[n] << " ";
+                std::cout << buffer_ptr->get(flatten_indices(i,j)) << " ";
                 n = n + 1;
             }
             std::cout << std::endl;
@@ -68,17 +98,20 @@ public:
 
     void set(int i, int j, double elem)
     {
-        buffer_ptr->tab[i + j] = elem;
+        buffer_ptr->set(flatten_indices(i,j), elem);
     }
 
-    int get(int i, int j)
+    double get(int i, int j)
     {
-        std::cout << buffer_ptr->tab[i + j] << std::endl; // pour debuger
-        return buffer_ptr->tab[i + j];
+        std::cout << buffer_ptr->get(flatten_indices(i, j)) << std::endl; // pour debuger
+        return buffer_ptr->get(flatten_indices(i, j));
     }
 
     Matrix reshape(int i, int j)
     {
+        if (i*j != rows*columns) {
+            throw std::invalid_argument("Reshape incompatible");
+        }
         Matrix m = Matrix(i, j, buffer_ptr);
         return m;
     }
@@ -96,7 +129,9 @@ int main()
 {
     Matrix m = Matrix(4, 3);
     m.set(0, 0, 17.2);
-    m.set(0, 3, 21.3);
+    m.set(0, 1, 21.3);
     m.print();
     return 0;
+    Matrix m2(2,4);
+    Matrix m3(m2); // Que se passe-t-il? même chose que Matrix m3= m2; mais le problème est que incr_nb_matrix ne sera pas appelé
 }
